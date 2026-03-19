08122fbc4da10ca1c48cce9aa29f52aebb57cb0f
Mrityunjay Goswami <47592090+MrityunjayGoswami@users.noreply.github.com>
2026-03-19 23:10:46 +0530

    Time: 0 ms (100%), Space: 8.4 MB (61.8%) - LeetHub

---
+++ 0628-maximum-product-of-three-numbers/0628-maximum-product-of-three-numbers.go
@@ -0,0 +1,34 @@
+func maximumProduct(nums []int) int {
+    max1, max2, max3 := -1001, -1001, -1001
+    min1, min2 := 1001, 1001
+
+    for _, num := range nums {
+        // Update max values
+        if num > max1 {
+            max3 = max2
+            max2 = max1
+            max1 = num
+        } else if num > max2 {
+            max3 = max2
+            max2 = num
+        } else if num > max3 {
+            max3 = num
+        }
+
+        // Update min values
+        if num < min1 {
+            min2 = min1
+            min1 = num
+        } else if num < min2 {
+            min2 = num
+        }
+    }
+
+    product1 := max1 * max2 * max3
+    product2 := min1 * min2 * max1
+
+    if product1 > product2 {
+        return product1
+    }
+    return product2
+}
